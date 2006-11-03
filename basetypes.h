86fe2614c858f8079a0f4b3992492516e20893d8
Anthony Williams <anthony@justsoftwaresolutions.co.uk>
2006-11-03 11:41:40 +0000

    added missing basetypes.h

---
+++ basetypes.h
@@ -0,0 +1,15 @@
+#ifndef BASETYPES_H
+#define BASETYPES_H
+
+typedef char CHAR,*PCHAR,**PPCHAR;
+typedef unsigned char UCHAR,*PUCHAR;
+typedef unsigned long UINT;
+typedef signed long INT;
+typedef unsigned short USHORT;
+typedef signed short SHORT;
+typedef int BOOL;
+
+#define TRUE  (1==1)
+#define FALSE (1==0)
+
+#endif
